da15b053d6890ff610e02719d5455aa68e3b0ebf
JayPokale <jay.pokale.35@gmail.com>
2023-08-22 12:39:10 +0530

    Editor Change

--- src/routes/quill.tsx
+++
@@ -1,9 +0,0 @@
-import { unstable_clientOnly } from "solid-start";
-
-const QuillEditor = unstable_clientOnly(() => import("~/components/Quill"));
-
-const quill = () => {
-  return <div>{typeof window !== "undefined" && <QuillEditor />}</div>;
-};
-
-export default quill;
